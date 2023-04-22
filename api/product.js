86d34446c830f25e6f7c8489089aeccff4b9c84b
Sandeep Nandanwar <sandeepnandanwar92@gmail.com>
2023-04-22 13:38:18 +0530

    post method added

--- api/product.js
+++ api/product.js
@@ -2,13 +2,23 @@ const express = require('express')
 const router = express.Router();
 
 
-router.get("/",async(req,res)=>{
+router.get("/list",async(req,res)=>{
     try {
         res.json({
             message:"Get"
         })
     } catch (error) {
         return res.status(500).send('errpr')
+    }
+})
+
+router.post("/post",async(req,res)=>{
+    try {
+        res.json({
+            message : "Get post method"
+        })
+    } catch (error) {
+        return res.status(400).json('error')
     }
 })
 
